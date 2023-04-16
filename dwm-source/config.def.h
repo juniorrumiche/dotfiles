d804c8f695d83222e2df6f09640cef58ecc0dfa8
Junior Rumiche <juniorrumichem@gmail.com>
2023-04-16 16:57:07 -0500

    increase gaps

--- dwm-source/config.def.h
+++ dwm-source/config.def.h
@@ -5,12 +5,12 @@ static const unsigned int default_border =
     1; // to switch back to default border after dynamic border resizing via
        // keybinds
 static const unsigned int snap = 32;  /* snap pixel */
-static const unsigned int gappih = 7; /* horiz inner gap between windows */ // division entre bordes
-static const unsigned int gappiv = 7; /* vert inner gap between windows */
+static const unsigned int gappih = 10; /* horiz inner gap between windows */ // division entre bordes
+static const unsigned int gappiv = 10; /* vert inner gap between windows */
 static const unsigned int gappoh =
-    6; /* horiz outer gap between windows and screen edge */
+    10; /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov =
-    6; /* vert outer gap between windows and screen edge */
+    10; /* vert outer gap between windows and screen edge */
 static int smartgaps =
     0; /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning =
