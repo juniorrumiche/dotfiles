3efe35529137a3be028b05f6ca6046f828e32a41
Junior Rumiche <juniorrumichem@gmail.com>
2023-06-07 22:37:04 -0500

    update font to st

--- st/config.def.h
+++ st/config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static  char *font = "JetBrainsMono Nerd Font Mono:pixelsize=14:autohint=true:style=Bold" ;
-static  char *font2[] = { "JetBrainsMono Nerd Font Mono:pixelsize=14:autohint=true:style=Bold" };
+static  char *font = "JetBrainsMono Nerd Font:style=Bold:pixelsize=14" ;
+static  char *font2[] = { "JetBrainsMono Nerd Font:style=Bold:pixelsize=14" };
 static int borderpx = 15;
 
 /*
