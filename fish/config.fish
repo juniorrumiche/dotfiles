d95d9874f3472df8e5d27db049fe934c77fab020
Junior Rumiche <juniorrumichem@gmail.com>
2023-09-26 23:14:03 -0500

    feat: add Android development environment setup
    Set up ANDROID_HOME, add platform tools to PATH.
    
    Add venv function to handle Python virtual environments.
    
    refactor: reorganize aliases
    
    Reorganize aliases into sections for Django, XAMPP, Git,
    and general aliases.
    
    Remove unused aliases, update Neovim aliases.

--- fish/config.fish
+++ fish/config.fish
@@ -1,26 +1,52 @@
+set -x ANDROID_HOME /opt/android-sdk
+set -x PATH $ANDROID_HOME/tools $PATH
+set -x PATH $ANDROID_HOME/tools/bin $PATH
+set -x PATH $ANDROID_HOME/platform-tools $PATH
+
+function venv
+    if not test -d venv
+        virtualenv venv
+    else
+        source venv/bin/activate.fish
+    end
+end
+
+
 if status is-interactive
-    alias pm="pnpm "
-    alias restart="sudo systemctl restart"
-    alias djrun="python manage.py runserver"
-    alias djstart="python manage.py startapp"
-    alias djmakemig="python manage.py makemigrations"
-    alias djmig="python manage.py migrate"
     # to clear command
+  
     alias clear="TERMINFO=/usr/share/terminfo TERM=xterm /usr/bin/clear"
-    
-    alias removeswap="rm -rf ~/.local/state/nvim"
+  
+    #django alias
+    alias djrun="python manage.py runserver"
+    alias djmakemigrations="python manage.py makemigrations"
+    alias djmigrate="python manage.py migrate"
+    alias djshell="python manage.py shell"
+    alias djtest="python manage.py test"
+    alias djcollectstatic="python manage.py collectstatic"
+    alias djsuperuser="python manage.py createsuperuser"
+    alias djcheck="python manage.py check"
+    alias djdumpdata="python manage.py dumpdata"
+    alias djloaddata="python manage.py loaddata"
+    # end djaango alias
+  
+    #xampp alias
     alias startxampp="sudo xampp startapache && sudo xampp startmysql"
     alias stopxampp="sudo xampp stop"
+  
+    alias pm="pnpm "
+    alias restart="sudo systemctl restart"
+    
+    alias removeswap="rm -rf ~/.local/state/nvim"
 
-    # for git
+    # git alias 
     alias gpush="git push origin"
     alias gswitch="git switch"
-
+  
     alias gpt="tgpt "
-
+  
     # my alias
-    alias v="nvim"
-    alias nv="neovide"
+    alias v="neovide "
     alias showkeys="screenkey  -s medium -p bottom --geometry 24%x50%+74%+50% --opacity 0.9 -M "
   
 end
